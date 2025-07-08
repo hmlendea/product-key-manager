5e73a7b0782ad2fc58fd1217316643ea35de0048
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-07-08 20:35:39 +0300

    nuciapi: Integrated with NuciAPI.Controllers

--- Api/Controllers/ProductKeysController.cs
+++ Api/Controllers/ProductKeysController.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Security;
 using Microsoft.AspNetCore.Mvc;
-using NuciAPI.Responses;
+using NuciAPI.Controllers;
 using ProductKeyManager.Api.Models;
 using ProductKeyManager.Service;
 
@@ -9,76 +7,18 @@ namespace ProductKeyManager.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ProductKeysController(IProductKeyService service) : ControllerBase
+    public class ProductKeysController(IProductKeyService service) : NuciApiController
     {
         [HttpGet]
         public ActionResult GetProductKey([FromBody] GetProductKeyRequest request)
-        {
-            if (request is null)
-            {
-                return BadRequest(NuciApiErrorResponse.InvalidRequest);
-            }
-
-            try
-            {
-                return Ok(service.GetProductKey(request));
-            }
-            catch (SecurityException ex)
-            {
-                return Unauthorized(NuciApiErrorResponse.FromException(ex));
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(NuciApiErrorResponse.FromException(ex));
-            }
-        }
+            => ProcessRequest(request, () => service.GetProductKey(request));
 
         [HttpPost]
         public ActionResult AddProductKey([FromBody] AddProductKeyRequest request)
-        {
-            if (request is null)
-            {
-                return BadRequest(NuciApiErrorResponse.InvalidRequest);
-            }
-
-            try
-            {
-                service.AddProductKey(request);
-
-                return Ok(NuciApiSuccessResponse.Default);
-            }
-            catch (SecurityException ex)
-            {
-                return Unauthorized(NuciApiErrorResponse.FromException(ex));
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(NuciApiErrorResponse.FromException(ex));
-            }
-        }
+            => ProcessRequest(request, () => service.AddProductKey(request));
 
         [HttpPut]
         public ActionResult UpdateProductKey([FromBody] UpdateProductKeyRequest request)
-        {
-            if (request is null)
-            {
-                return BadRequest(NuciApiErrorResponse.InvalidRequest);
-            }
-
-            try
-            {
-                service.UpdateProductKey(request);
-
-                return Ok(NuciApiSuccessResponse.Default);
-            }
-            catch (SecurityException ex)
-            {
-                return Unauthorized(NuciApiErrorResponse.FromException(ex));
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(NuciApiErrorResponse.FromException(ex));
-            }
-        }
+            => ProcessRequest(request, () => service.UpdateProductKey(request));
     }
 }
