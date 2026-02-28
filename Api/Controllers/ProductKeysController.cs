ae33811b67aaf05ab542d8c9021e624ae39234b1
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-02-28 21:03:05 +0200

    auth: Implemented API-Key authorisation

--- Api/Controllers/ProductKeysController.cs
+++ Api/Controllers/ProductKeysController.cs
@@ -1,24 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using NuciAPI.Controllers;
 using ProductKeyManager.Api.Models;
+using ProductKeyManager.Configuration;
 using ProductKeyManager.Service;
 
 namespace ProductKeyManager.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ProductKeysController(IProductKeyService service) : NuciApiController
+    public class ProductKeysController(
+        IProductKeyService service,
+        SecuritySettings securitySettings) : NuciApiController
     {
+        readonly NuciApiAuthorisation authorisation = NuciApiAuthorisation.ApiKey(securitySettings.SharedSecretKey);
+
         [HttpGet]
         public ActionResult GetProductKey([FromBody] GetProductKeyRequest request)
-            => ProcessRequest(request, () => service.GetProductKey(request));
+            => ProcessRequest(request, () => service.GetProductKey(request), authorisation);
 
         [HttpPost]
         public ActionResult AddProductKey([FromBody] AddProductKeyRequest request)
-            => ProcessRequest(request, () => service.AddProductKey(request));
+            => ProcessRequest(request, () => service.AddProductKey(request), authorisation);
 
         [HttpPut]
         public ActionResult UpdateProductKey([FromBody] UpdateProductKeyRequest request)
-            => ProcessRequest(request, () => service.UpdateProductKey(request));
+            => ProcessRequest(request, () => service.UpdateProductKey(request), authorisation);
     }
 }
