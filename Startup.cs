89bcd670a3e942414fb09eb8253b40a1951cdaf7
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-28 16:08:30 +0300

    deps: Updated the dependencies

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NuciAPI.Middleware;
+using NuciAPI.Middleware.ExceptionHandling;
+using NuciAPI.Middleware.Logging;
+using NuciAPI.Middleware.Security;
 using ProductKeyManager.Configuration;
 
 namespace ProductKeyManager
